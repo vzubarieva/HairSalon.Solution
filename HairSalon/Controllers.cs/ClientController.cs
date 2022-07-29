d33ed6b53dd6478b8219d2ba6fda7414489e5acc
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-28 23:02:25 -0700

    add edit functionality

--- HairSalon/Controllers.cs/ClientController.cs
+++ HairSalon/Controllers.cs/ClientController.cs
@@ -40,5 +40,19 @@ namespace HairSalon.Controllers
             Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
             return View(thisClient);
         }
+
+        public ActionResult Edit(int id)
+        {
+            var thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+            return View(thisClient);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Client client)
+        {
+            _db.Entry(client).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
