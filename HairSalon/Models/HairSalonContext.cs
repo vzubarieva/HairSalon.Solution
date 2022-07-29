079604f2c6296b05281abb2ccd0b3e6bae5fe1d2
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-28 23:34:15 -0700

    update client views

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,7 +4,7 @@ namespace HairSalon.Models
 {
     public class HairSalonContext : DbContext
     {
-        //public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Stylist> Stylist { get; set; }
         public DbSet<Client> Clients { get; set; }
 
         public HairSalonContext(DbContextOptions options) : base(options) { }
